2e76f40affac363438f12c59d897027d257c2989
권성준 <58690800+KownSungJun@users.noreply.github.com>
2025-10-26 14:58:16 +0000

    넷플릭스 2일차

--- README.md
+++ README.md
@@ -3,6 +3,6 @@
 ## 넷플릭스
 시작하기
 1. 프로젝트 폴더 생성 `npm create vite@latest netflix --template react`
-2. 
+2. 프로젝트 라이브 `npm run dev`
 
 
---
+++ netflix/src/components/AdCard.jsx
@@ -0,0 +1,80 @@
+function AdCard() {
+    return (
+        <>
+            <div dir="ltr" className="default-ltr-iqcdef-cache-bz4hbi e11rzblw1">
+      <div data-layout="wrapper" className="default-ltr-iqcdef-cache-1olxzr7">
+        <div
+          data-layout="container"
+          className="default-ltr-iqcdef-cache-12ymzig"
+          style={{
+            "--wct--layout-container--alignItems-xs": "flex-start",
+            "--wct--layout-container--alignItems-s": "center",
+            "--wct--layout-container--alignItems-m": "center",
+            "--wct--layout-container--alignItems-l": "center",
+            "--wct--layout-container--alignItems-xl": "center",
+            "--wct--layout-container--columnSpacing": "1rem",
+            "--wct--layout-container--flexDirection-xs": "column",
+            "--wct--layout-container--flexDirection-s": "row",
+            "--wct--layout-container--flexDirection-m": "row",
+            "--wct--layout-container--flexDirection-l": "row",
+            "--wct--layout-container--flexDirection-xl": "row",
+            "--wct--layout-container--justifyContent": "space-between",
+            "--wct--layout-container--padding-xs": "2rem 1rem",
+            "--wct--layout-container--padding-s": "0.75rem 1.5rem",
+            "--wct--layout-container--padding-m": "0.88rem 1.5em",
+            "--wct--layout-container--padding-l": "1rem 2rem",
+            "--wct--layout-container--padding-xl": "1rem 2.5rem",
+            "--wct--layout-container--rowSpacing": "1rem",
+            "--wct--layout-container--width": "calc(100% + 1rem)",
+          }}
+        >
+          <div
+            className="default-ltr-iqcdef-cache-rv48g6"
+            data-layout="item"
+            style={{
+              "--wct--layout-item--padding": "0px",
+              "--wct--layout-item--width-xs": "fit-content",
+              "--wct--layout-item--width-s": "auto",
+              "--wct--layout-item--width-m": "auto",
+              "--wct--layout-item--width-l": "auto",
+              "--wct--layout-item--width-xl": "auto",
+            }}
+          >
+            <div>
+              <h3 className="default-ltr-iqcdef-cache-1skz2f4 eb5pmcc0">
+                7,000원이면 만날 수 있는 넷플릭스.
+              </h3>
+              <p className="default-ltr-iqcdef-cache-6ukeej eb5pmcc0">
+                가장 경제적인 광고형 멤버십을 이용해 보세요.
+              </p>
+            </div>
+          </div>
+
+          <div
+            className="default-ltr-iqcdef-cache-rv48g6"
+            data-layout="item"
+            style={{
+              "--wct--layout-item--padding": "0px",
+              "--wct--layout-item--width-xs": "fit-content",
+              "--wct--layout-item--width-s": "auto",
+              "--wct--layout-item--width-m": "auto",
+              "--wct--layout-item--width-l": "auto",
+              "--wct--layout-item--width-xl": "auto",
+            }}
+          >
+            <button
+              className="ea2wixt2 default-ltr-iqcdef-cache-1gb3zrv"
+              data-uia="promo-banner-cta"
+              type="button"
+            >
+              자세히 알아보기
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+        </>
+    )
+}
+
+export default AdCard
